9e530373faf846b5f7545821bb2c6f2403a6894f
FlorianVeaux <florian.veaux@datadoghq.com>
2021-04-01 12:58:02 +0200

    Move configuration into its own class

--- src/main/java/org/datadog/jmeter/plugins/DatadogBackendClient.java
+++ src/main/java/org/datadog/jmeter/plugins/DatadogBackendClient.java
@@ -24,6 +24,8 @@ import org.apache.jmeter.visualizers.backend.AbstractBackendListenerClient;
 import org.apache.jmeter.visualizers.backend.BackendListenerContext;
 import org.apache.jmeter.visualizers.backend.UserMetric;
 import org.datadog.jmeter.plugins.aggregation.ConcurrentAggregator;
+import org.datadog.jmeter.plugins.exceptions.DatadogApiException;
+import org.datadog.jmeter.plugins.exceptions.DatadogConfigurationException;
 import org.datadog.jmeter.plugins.metrics.DatadogMetric;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -47,6 +49,12 @@ public class DatadogBackendClient extends AbstractBackendListenerClient implemen
     private DatadogHttpClient datadogClient;
 
     /**
+     * An instance of {@link DatadogConfiguration}.
+     * Instantiated during the test set up phase and contains the plugin configuration.
+     */
+    private DatadogConfiguration configuration;
+
+    /**
      * An instance of {@link ConcurrentAggregator}.
      * Instantiated upon creation of the DatadogBackendClient class. Aggregates metrics until instructed to flush.
      * Flushing occurs at a fixed schedule rate, @see {@link #timerHandle} and {@link #METRICS_SEND_INTERVAL}.
@@ -55,7 +63,7 @@ public class DatadogBackendClient extends AbstractBackendListenerClient implemen
 
     /**
      * An list of JSON log payloads to buffer calls to the Datadog API. Unlike metrics, logs are not aggregated before being sent. Thus
-     * flushing of logs doesn't occur at a fixed time interval but rather once the buffer is bigger than {@link #logsBatchSize}.
+     * flushing of logs doesn't occur at a fixed time interval but rather once the buffer is bigger than {@link DatadogConfiguration#getLogsBatchSize()}.
      */
     private List<JSONObject> logsBuffer = new ArrayList<>();
 
@@ -76,56 +84,6 @@ public class DatadogBackendClient extends AbstractBackendListenerClient implemen
      */
     private ScheduledFuture<?> timerHandle;
 
-
-    /**
-     * User configurable. This option configures how many metrics are sent in the same http request to Datadog.
-     * NOTE: Metrics are always sent at a fixed interval. This field configures how many API calls will be perfomed at the end of
-     * said interval.
-     */
-    private int metricsBatchSize = DEFAULT_METRICS_BATCH_SIZE;
-
-    /**
-     * User configurable. This option configures the size of the logs buffer. The bigger the value, the more logs will be kept in memory
-     * before being sent to Datadog and the bigger the resulting api call will be. Once that buffer size is reached, all pending log events
-     * are sent.
-     */
-    private int logsBatchSize = DEFAULT_LOGS_BATCH_SIZE;
-
-    /**
-     * User configurable. This options configures whether or not the plugin should submit individual results as Datadog logs.
-     */
-    private boolean sendResultsAsLogs = DEFAULT_SEND_RESULTS_AS_LOGS;
-
-    /**
-     * User configurable. This options configures whether or not to collect metrics and logs for jmeter subresults.
-     */
-    private boolean includeSubResults = DEFAULT_INCLUDE_SUB_RESULTS;
-
-    /**
-     * User configurable. This options configures which samplers to include in monitoring results using a regex
-     * that matches on the sampler name.
-     */
-    private Pattern samplersRegex = null;
-
-    /* The names of configuration options that are shown in JMeter UI */
-    private static final String API_URL_PARAM = "datadogUrl";
-    private static final String LOG_INTAKE_URL_PARAM = "logIntakeUrl";
-    private static final String API_KEY_PARAM = "apiKey";
-    private static final String METRICS_BATCH_SIZE = "metricsMaxBatchSize";
-    private static final String LOGS_BATCH_SIZE = "logsBatchSize";
-    private static final String SEND_RESULTS_AS_LOGS = "sendResultsAsLogs";
-    private static final String INCLUDE_SUB_RESULTS = "includeSubresults";
-    private static final String SAMPLERS_REGEX = "samplersRegex";
-
-    /* The default values for all configuration options */
-    private static final String DEFAULT_API_URL = "https://api.datadoghq.com/api/";
-    private static final String DEFAULT_LOG_INTAKE_URL = "https://http-intake.logs.datadoghq.com/v1/input/";
-    private static final int DEFAULT_METRICS_BATCH_SIZE = 200;
-    private static final int DEFAULT_LOGS_BATCH_SIZE = 500;
-    private static final boolean DEFAULT_SEND_RESULTS_AS_LOGS = true;
-    private static final boolean DEFAULT_INCLUDE_SUB_RESULTS = false;
-    private static final String DEFAULT_SAMPLERS_REGEX = "";
-
     /**
      * Calls at a fixed schedule and sends metrics to Datadog.
      */
@@ -140,62 +98,24 @@ public class DatadogBackendClient extends AbstractBackendListenerClient implemen
      */
     @Override
     public Arguments getDefaultParameters() {
-        Arguments arguments = new Arguments();
-        arguments.addArgument(API_KEY_PARAM, null);
-        arguments.addArgument(API_URL_PARAM, DEFAULT_API_URL);
-        arguments.addArgument(LOG_INTAKE_URL_PARAM, DEFAULT_LOG_INTAKE_URL);
-        arguments.addArgument(METRICS_BATCH_SIZE, String.valueOf(DEFAULT_METRICS_BATCH_SIZE));
-        arguments.addArgument(LOGS_BATCH_SIZE, String.valueOf(DEFAULT_LOGS_BATCH_SIZE));
-        arguments.addArgument(SEND_RESULTS_AS_LOGS, String.valueOf(DEFAULT_SEND_RESULTS_AS_LOGS));
-        arguments.addArgument(INCLUDE_SUB_RESULTS, String.valueOf(DEFAULT_INCLUDE_SUB_RESULTS));
-        arguments.addArgument(SAMPLERS_REGEX, DEFAULT_SAMPLERS_REGEX);
-
-        return arguments;
+        return DatadogConfiguration.getPluginArguments();
     }
 
     /**
      * Called before starting the test.
      * @param context An object used to fetch user configuration.
-     * @throws Exception If the configuration is invalid or if the plugin can't connect to Datadog.
+     * @throws DatadogConfigurationException If the configuration is invalid.
+     * @throws DatadogApiException If the plugin can't connect to Datadog.
      */
     @Override
     public void setupTest(BackendListenerContext context) throws Exception {
-        String apiKey = context.getParameter(API_KEY_PARAM);
-        String apiUrl = context.getParameter(API_URL_PARAM, DEFAULT_API_URL);
-        String logIntakeUrl = context.getParameter(LOG_INTAKE_URL_PARAM, DEFAULT_LOG_INTAKE_URL);
-        String metricsBatchSize = context.getParameter(METRICS_BATCH_SIZE, String.valueOf(DEFAULT_METRICS_BATCH_SIZE));
-        String logsBatchSize = context.getParameter(LOGS_BATCH_SIZE, String.valueOf(DEFAULT_LOGS_BATCH_SIZE));
-        String sendResultsAsLogs = context.getParameter(SEND_RESULTS_AS_LOGS, String.valueOf(DEFAULT_SEND_RESULTS_AS_LOGS));
-        String samplersRegex = context.getParameter(SAMPLERS_REGEX, DEFAULT_SAMPLERS_REGEX);
+        this.configuration = DatadogConfiguration.parseConfiguration(context);
 
-        if (apiKey == null) {
-            throw new Exception("apiKey needs to be configured.");
-        }
-
-        datadogClient = new DatadogHttpClient(apiKey, apiUrl, logIntakeUrl);
+        datadogClient = new DatadogHttpClient(configuration.getApiKey(), configuration.getApiUrl(), configuration.getLogIntakeUrl());
         boolean valid = datadogClient.validateConnection();
         if(!valid) {
-            throw new Exception("Invalid apiKey");
+            throw new DatadogApiException("Invalid apiKey");
         }
-
-        try {
-            this.metricsBatchSize = Integer.parseUnsignedInt(metricsBatchSize);
-        } catch (NumberFormatException e) {
-            throw new Exception("Invalid 'metricsBatchSize'. Value '" + metricsBatchSize + "' is not an integer.");
-        }
-
-        try {
-            this.logsBatchSize = Integer.parseUnsignedInt(logsBatchSize);
-        } catch (NumberFormatException e) {
-            throw new Exception("Invalid 'logsBatchSize'. Value '" + logsBatchSize + "' is not an integer.");
-        }
-
-        if(!sendResultsAsLogs.toLowerCase().equals("false") && !sendResultsAsLogs.toLowerCase().equals("true")) {
-            throw new Exception("Invalid 'sendResultsAsLogs'. Value '" + sendResultsAsLogs + "' is not a boolean.");
-        }
-        this.sendResultsAsLogs = Boolean.parseBoolean(sendResultsAsLogs);
-        this.samplersRegex = Pattern.compile(samplersRegex);
-        this.includeSubResults = context.getBooleanParameter(INCLUDE_SUB_RESULTS, DEFAULT_INCLUDE_SUB_RESULTS);
 
         scheduler = Executors.newScheduledThreadPool(1);
         this.timerHandle = scheduler.scheduleAtFixedRate(this, METRICS_SEND_INTERVAL, METRICS_SEND_INTERVAL, TimeUnit.SECONDS);
@@ -236,16 +156,15 @@ public class DatadogBackendClient extends AbstractBackendListenerClient implemen
     @Override
     public void handleSampleResults(List<SampleResult> list, BackendListenerContext backendListenerContext) {
         for (SampleResult sampleResult : list) {
-            Matcher matcher = samplersRegex.matcher(sampleResult.getSampleLabel());
+            Matcher matcher = configuration.getSamplersRegex().matcher(sampleResult.getSampleLabel());
             if(!matcher.find()) {
                 continue;
             }
-            if(this.includeSubResults) {
+            if(configuration.shouldIncludeSubResults()) {
                 for (SampleResult subResult : sampleResult.getSubResults()) {
                     this.extractData(subResult);
                 }
             } else {
-                // TODO: Should we also include this one even when includeSubResults is set?
                 this.extractData(sampleResult);
             }
         }
@@ -259,9 +178,9 @@ public class DatadogBackendClient extends AbstractBackendListenerClient implemen
         UserMetric userMetrics = this.getUserMetrics();
         userMetrics.add(sampleResult);
         this.extractMetrics(sampleResult);
-        if(this.sendResultsAsLogs) {
+        if(configuration.shouldSendResultsAsLogs()) {
             this.extractLogs(sampleResult);
-            if(logsBuffer.size() >= this.logsBatchSize) {
+            if(logsBuffer.size() >= configuration.getLogsBatchSize()) {
                 datadogClient.submitLogs(logsBuffer);
                 logsBuffer.clear();
             }
@@ -284,20 +203,10 @@ public class DatadogBackendClient extends AbstractBackendListenerClient implemen
             aggregator.incrementCounter("jmeter.responses_count", tags, sampleResult.getErrorCount());
         }
 
-        for(SampleResult r : sampleResult.getSubResults()) {
-            if(sampleResult.isSuccessful()) {
-                aggregator.incrementCounter("jmeter.responses_count", tags, sampleResult.getSampleCount() - sampleResult.getErrorCount());
-            } else {
-                aggregator.incrementCounter("jmeter.responses_count", tags, sampleResult.getErrorCount());
-            }
-        }
-        // TODO: Add responses count for each sub result
-        // TODO: Add request per second
         aggregator.histogram("jmeter.response_time", tags, sampleResult.getTime() / 1000f);
         aggregator.histogram("jmeter.bytes_sent", tags, sampleResult.getSentBytes());
         aggregator.histogram("jmeter.bytes_received", tags, sampleResult.getBytesAsLong());
         aggregator.histogram("jmeter.latency", tags, sampleResult.getLatency() / 1000f);
-
     }
 
     /**
@@ -356,7 +265,7 @@ public class DatadogBackendClient extends AbstractBackendListenerClient implemen
         List<DatadogMetric> metrics = aggregator.flushMetrics();
 
         AtomicInteger counter = new AtomicInteger();
-        metrics.stream().collect(Collectors.groupingBy(it -> counter.getAndIncrement() / this.metricsBatchSize)).values().forEach(
+        metrics.stream().collect(Collectors.groupingBy(it -> counter.getAndIncrement() / configuration.getMetricsMaxBatchSize())).values().forEach(
                 x -> datadogClient.submitMetrics(x)
         );
     }
---
+++ src/main/java/org/datadog/jmeter/plugins/DatadogConfiguration.java
@@ -0,0 +1,165 @@
+package org.datadog.jmeter.plugins;
+
+import java.util.regex.Pattern;
+import org.apache.jmeter.config.Arguments;
+import org.apache.jmeter.visualizers.backend.BackendListenerContext;
+import org.datadog.jmeter.plugins.exceptions.DatadogConfigurationException;
+
+
+public class DatadogConfiguration {
+
+    /**
+     * The Datadog api key to use for submitting metrics and logs.
+     */
+    private String apiKey;
+
+    /**
+     * The Datadog api url to use for submitting metrics.
+     */
+    private String apiUrl;
+
+    /**
+     * The Datadog api url to use for submitting logs.
+     */
+    private String logIntakeUrl;
+
+    /**
+     * This option configures how many metrics are sent in the same http request to Datadog.
+     * NOTE: Metrics are always sent at a fixed interval. This field configures how many API calls will be perfomed at the end of
+     * said interval.
+     */
+    private int metricsMaxBatchSize;
+
+    /**
+     * This option configures the size of the logs buffer. The bigger the value, the more logs will be kept in memory
+     * before being sent to Datadog and the bigger the resulting api call will be. Once that buffer size is reached, all pending log events
+     * are sent.
+     */
+    private int logsBatchSize;
+
+    /**
+     * This options configures whether or not the plugin should submit individual results as Datadog logs.
+     */
+    private boolean sendResultsAsLogs;
+
+    /**
+     * This options configures whether or not to collect metrics and logs for jmeter subresults.
+     */
+    private boolean includeSubResults;
+
+    /**
+     * User configurable. This options configures which samplers to include in monitoring results using a regex
+     * that matches on the sampler name.
+     */
+    private Pattern samplersRegex = null;
+
+    /* The names of configuration options that are shown in JMeter UI */
+    private static final String API_URL_PARAM = "datadogUrl";
+    private static final String LOG_INTAKE_URL_PARAM = "logIntakeUrl";
+    private static final String API_KEY_PARAM = "apiKey";
+    private static final String METRICS_MAX_BATCH_SIZE = "metricsMaxBatchSize";
+    private static final String LOGS_BATCH_SIZE = "logsBatchSize";
+    private static final String SEND_RESULTS_AS_LOGS = "sendResultsAsLogs";
+    private static final String INCLUDE_SUB_RESULTS = "includeSubresults";
+    private static final String SAMPLERS_REGEX = "samplersRegex";
+
+    /* The default values for all configuration options */
+    private static final String DEFAULT_API_URL = "https://api.datadoghq.com/api/";
+    private static final String DEFAULT_LOG_INTAKE_URL = "https://http-intake.logs.datadoghq.com/v1/input/";
+    private static final int DEFAULT_METRICS_MAX_BATCH_SIZE = 200;
+    private static final int DEFAULT_LOGS_BATCH_SIZE = 500;
+    private static final boolean DEFAULT_SEND_RESULTS_AS_LOGS = true;
+    private static final boolean DEFAULT_INCLUDE_SUB_RESULTS = false;
+    private static final String DEFAULT_SAMPLERS_REGEX = "";
+
+    private DatadogConfiguration(){}
+
+    public static Arguments getPluginArguments() {
+        Arguments arguments = new Arguments();
+        arguments.addArgument(API_KEY_PARAM, null);
+        arguments.addArgument(API_URL_PARAM, DEFAULT_API_URL);
+        arguments.addArgument(LOG_INTAKE_URL_PARAM, DEFAULT_LOG_INTAKE_URL);
+        arguments.addArgument(METRICS_MAX_BATCH_SIZE, String.valueOf(DEFAULT_METRICS_MAX_BATCH_SIZE));
+        arguments.addArgument(LOGS_BATCH_SIZE, String.valueOf(DEFAULT_LOGS_BATCH_SIZE));
+        arguments.addArgument(SEND_RESULTS_AS_LOGS, String.valueOf(DEFAULT_SEND_RESULTS_AS_LOGS));
+        arguments.addArgument(INCLUDE_SUB_RESULTS, String.valueOf(DEFAULT_INCLUDE_SUB_RESULTS));
+        arguments.addArgument(SAMPLERS_REGEX, DEFAULT_SAMPLERS_REGEX);
+
+        return arguments;
+    }
+
+    public static DatadogConfiguration parseConfiguration(BackendListenerContext context) throws DatadogConfigurationException {
+        DatadogConfiguration configuration = new DatadogConfiguration();
+
+        String apiKey = context.getParameter(API_KEY_PARAM);
+        if (apiKey == null) {
+            throw new DatadogConfigurationException("apiKey needs to be configured.");
+        }
+        configuration.apiKey = apiKey;
+
+        configuration.apiUrl = context.getParameter(API_URL_PARAM, DEFAULT_API_URL);
+        configuration.logIntakeUrl = context.getParameter(LOG_INTAKE_URL_PARAM, DEFAULT_LOG_INTAKE_URL);
+
+
+        String metricsMaxBatchSize = context.getParameter(METRICS_MAX_BATCH_SIZE, String.valueOf(DEFAULT_METRICS_MAX_BATCH_SIZE));
+        try {
+            configuration.metricsMaxBatchSize = Integer.parseUnsignedInt(metricsMaxBatchSize);
+        } catch (NumberFormatException e) {
+            throw new DatadogConfigurationException("Invalid 'metricsMaxBatchSize'. Value '" + metricsMaxBatchSize + "' is not an integer.");
+        }
+
+        String logsBatchSize = context.getParameter(LOGS_BATCH_SIZE, String.valueOf(DEFAULT_LOGS_BATCH_SIZE));
+        try {
+            configuration.logsBatchSize = Integer.parseUnsignedInt(logsBatchSize);
+        } catch (NumberFormatException e) {
+            throw new DatadogConfigurationException("Invalid 'logsBatchSize'. Value '" + logsBatchSize + "' is not an integer.");
+        }
+
+        String sendResultsAsLogs = context.getParameter(SEND_RESULTS_AS_LOGS, String.valueOf(DEFAULT_SEND_RESULTS_AS_LOGS));
+        if(!sendResultsAsLogs.toLowerCase().equals("false") && !sendResultsAsLogs.toLowerCase().equals("true")) {
+            throw new DatadogConfigurationException("Invalid 'sendResultsAsLogs'. Value '" + sendResultsAsLogs + "' is not a boolean.");
+        }
+        configuration.sendResultsAsLogs = Boolean.parseBoolean(sendResultsAsLogs);
+
+        String includeSubResults = context.getParameter(INCLUDE_SUB_RESULTS, String.valueOf(DEFAULT_INCLUDE_SUB_RESULTS));
+        if(!includeSubResults.toLowerCase().equals("false") && !includeSubResults.toLowerCase().equals("true")) {
+            throw new DatadogConfigurationException("Invalid 'includeSubResults'. Value '" + includeSubResults + "' is not a boolean.");
+        }
+        configuration.includeSubResults = Boolean.parseBoolean(includeSubResults);
+        configuration.samplersRegex = Pattern.compile(context.getParameter(SAMPLERS_REGEX, DEFAULT_SAMPLERS_REGEX));
+
+        return configuration;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public String getApiUrl() {
+        return apiUrl;
+    }
+
+    public String getLogIntakeUrl() {
+        return logIntakeUrl;
+    }
+
+    public int getMetricsMaxBatchSize() {
+        return metricsMaxBatchSize;
+    }
+
+    public int getLogsBatchSize() {
+        return logsBatchSize;
+    }
+
+    public boolean shouldSendResultsAsLogs() {
+        return sendResultsAsLogs;
+    }
+
+    public boolean shouldIncludeSubResults() {
+        return includeSubResults;
+    }
+
+    public Pattern getSamplersRegex() {
+        return samplersRegex;
+    }
+}
