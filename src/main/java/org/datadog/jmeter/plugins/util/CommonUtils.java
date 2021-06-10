b7d99e11ad70680fb129b40be6b781ae0e657920
Ofek Lev <ofekmeister@gmail.com>
2021-06-10 10:30:42 -0400

    Add `thread_group` to log payload (#18)
    * Add `thread_group` to log payload
    
    * Apply suggestions from code review
    
    Co-authored-by: Julia <611228+hithwen@users.noreply.github.com>
    
    * Revert "Apply suggestions from code review"
    
    This reverts commit 0a237453e73e77590ef01714798ed1df015c7040.
    
    Co-authored-by: Julia <611228+hithwen@users.noreply.github.com>

---
+++ src/main/java/org/datadog/jmeter/plugins/util/CommonUtils.java
@@ -0,0 +1,10 @@
+package org.datadog.jmeter.plugins.util;
+
+public class CommonUtils {
+
+    public static String parseThreadGroup(String threadName) {
+        // https://github.com/apache/jmeter/pull/622
+        return threadName.substring(0, Math.max(0, threadName.lastIndexOf(" ")));
+    }
+
+}
--- src/test/java/org/datadog/jmeter/plugins/DatadogBackendClientTest.java
+++ src/test/java/org/datadog/jmeter/plugins/DatadogBackendClientTest.java
@@ -148,7 +148,7 @@ public class DatadogBackendClientTest
         SampleResult result = createDummySampleResult("foo");
         this.client.handleSampleResults(Collections.singletonList(result), context);
         Assert.assertEquals(1, this.logsBuffer.size());
-        String expectedPayload = "{\"sample_start_time\":1.0,\"response_code\":\"123\",\"headers_size\":0.0,\"sample_label\":\"foo\",\"latency\":12.0,\"group_threads\":0.0,\"idle_time\":0.0,\"error_count\":0.0,\"message\":\"\",\"url\":\"\",\"ddsource\":\"jmeter\",\"sent_bytes\":124.0,\"body_size\":0.0,\"content_type\":\"\",\"load_time\":125.0,\"thread_name\":\"bar baz\",\"sample_end_time\":126.0,\"bytes\":12345.0,\"connect_time\":0.0,\"sample_count\":10.0,\"data_type\":\"\",\"all_threads\":0.0,\"data_encoding\":null}";
+        String expectedPayload = "{\"sample_start_time\":1.0,\"response_code\":\"123\",\"headers_size\":0.0,\"sample_label\":\"foo\",\"latency\":12.0,\"group_threads\":0.0,\"idle_time\":0.0,\"error_count\":0.0,\"message\":\"\",\"url\":\"\",\"ddsource\":\"jmeter\",\"sent_bytes\":124.0,\"thread_group\":\"bar\",\"body_size\":0.0,\"content_type\":\"\",\"load_time\":125.0,\"thread_name\":\"bar baz\",\"sample_end_time\":126.0,\"bytes\":12345.0,\"connect_time\":0.0,\"sample_count\":10.0,\"data_type\":\"\",\"all_threads\":0.0,\"data_encoding\":null}";
         Assert.assertEquals(this.logsBuffer.get(0).toString(), expectedPayload);
     }
 
