651e2e789655a55afd096cd483a261feffcf8316
Yihen Liu <yichen28@illinois.edu>
2022-11-24 20:36:30 -0600

    use minidev json parser

--- src/test/java/org/datadog/jmeter/plugins/DatadogBackendClientTest.java
+++ src/test/java/org/datadog/jmeter/plugins/DatadogBackendClientTest.java
@@ -13,7 +13,10 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.io.IOException;
 import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
+import net.minidev.json.parser.ParseException;
 import org.apache.jmeter.samplers.SampleResult;
 import org.apache.jmeter.visualizers.backend.BackendListenerContext;
 import org.datadog.jmeter.plugins.aggregation.ConcurrentAggregator;
@@ -27,7 +30,6 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import com.google.gson.JsonParser;
 
 /**
  * Unit test for simple App.
@@ -170,13 +172,18 @@ public class DatadogBackendClientTest
     }
 
     @Test
-    public void testExtractLogs() {
+    public void testExtractLogs() throws IOException{
         SampleResult result = createDummySampleResult("foo");
         this.client.handleSampleResults(Collections.singletonList(result), context);
         Assert.assertEquals(1, this.logsBuffer.size());
-        JsonParser parser = new JsonParser();
         String expectedPayload = "{\"sample_start_time\":1.0,\"response_code\":\"123\",\"headers_size\":0.0,\"sample_label\":\"foo\",\"latency\":12.0,\"group_threads\":0.0,\"idle_time\":0.0,\"error_count\":0.0,\"message\":\"\",\"url\":\"\",\"ddsource\":\"jmeter\",\"sent_bytes\":124.0,\"thread_group\":\"bar\",\"body_size\":0.0,\"content_type\":\"\",\"load_time\":125.0,\"thread_name\":\"bar baz\",\"sample_end_time\":126.0,\"bytes\":12345.0,\"connect_time\":0.0,\"sample_count\":10.0,\"data_type\":\"\",\"all_threads\":0.0,\"data_encoding\":null}";
-        Assert.assertEquals(parser.parse(this.logsBuffer.get(0).toString()), parser.parse(expectedPayload));
+        JSONParser parser = new JSONParser();
+        try {
+            Assert.assertEquals(parser.parse(this.logsBuffer.get(0).toString()), parser.parse(expectedPayload));
+        }
+        catch (ParseException ex) {
+            throw new IOException("Error while parsing payload from testExtractLogs ", ex);
+        }
     }
 
     @Test
