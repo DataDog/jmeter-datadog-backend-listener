395442c896572d75ce9e9b3873a4e410275f9814
Yihen Liu <yichen28@illinois.edu>
2022-11-25 11:33:16 -0600

    improved the fix

--- src/test/java/org/datadog/jmeter/plugins/DatadogBackendClientTest.java
+++ src/test/java/org/datadog/jmeter/plugins/DatadogBackendClientTest.java
@@ -13,7 +13,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.io.IOException;
 import net.minidev.json.JSONObject;
 import net.minidev.json.parser.JSONParser;
 import net.minidev.json.parser.ParseException;
@@ -172,18 +171,13 @@ public class DatadogBackendClientTest
     }
 
     @Test
-    public void testExtractLogs() throws IOException{
+    public void testExtractLogs() throws ParseException{
         SampleResult result = createDummySampleResult("foo");
         this.client.handleSampleResults(Collections.singletonList(result), context);
         Assert.assertEquals(1, this.logsBuffer.size());
         String expectedPayload = "{\"sample_start_time\":1.0,\"response_code\":\"123\",\"headers_size\":0.0,\"sample_label\":\"foo\",\"latency\":12.0,\"group_threads\":0.0,\"idle_time\":0.0,\"error_count\":0.0,\"message\":\"\",\"url\":\"\",\"ddsource\":\"jmeter\",\"sent_bytes\":124.0,\"thread_group\":\"bar\",\"body_size\":0.0,\"content_type\":\"\",\"load_time\":125.0,\"thread_name\":\"bar baz\",\"sample_end_time\":126.0,\"bytes\":12345.0,\"connect_time\":0.0,\"sample_count\":10.0,\"data_type\":\"\",\"all_threads\":0.0,\"data_encoding\":null}";
-        JSONParser parser = new JSONParser();
-        try {
-            Assert.assertEquals(parser.parse(this.logsBuffer.get(0).toString()), parser.parse(expectedPayload));
-        }
-        catch (ParseException ex) {
-            throw new IOException("Error while parsing payload from testExtractLogs ", ex);
-        }
+        JSONParser parser = new JSONParser(JSONParser.MODE_PERMISSIVE);
+        Assert.assertEquals(this.logsBuffer.get(0), parser.parse(expectedPayload));
     }
 
     @Test
