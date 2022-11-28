5c97b62c01780b5da17c987111d0ad26144bed7f
tt0suzy <tt0suzy@gmail.com>
2022-11-28 03:26:58 -0600

    fixed flaky test in DataDogBackendClientTest (#33)
    * fixed flaky test in DataDogBackendClientTest
    
    * use minidev json parser
    
    * improved the fix
    
    * Update src/test/java/org/datadog/jmeter/plugins/DatadogBackendClientTest.java
    
    Co-authored-by: Florent Clarret <florent.clarret@gmail.com>

--- src/test/java/org/datadog/jmeter/plugins/DatadogBackendClientTest.java
+++ src/test/java/org/datadog/jmeter/plugins/DatadogBackendClientTest.java
@@ -14,6 +14,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
+import net.minidev.json.parser.ParseException;
 import org.apache.jmeter.samplers.SampleResult;
 import org.apache.jmeter.visualizers.backend.BackendListenerContext;
 import org.datadog.jmeter.plugins.aggregation.ConcurrentAggregator;
@@ -169,12 +171,13 @@ public class DatadogBackendClientTest
     }
 
     @Test
-    public void testExtractLogs() {
+    public void testExtractLogs() throws ParseException {
         SampleResult result = createDummySampleResult("foo");
         this.client.handleSampleResults(Collections.singletonList(result), context);
         Assert.assertEquals(1, this.logsBuffer.size());
         String expectedPayload = "{\"sample_start_time\":1.0,\"response_code\":\"123\",\"headers_size\":0.0,\"sample_label\":\"foo\",\"latency\":12.0,\"group_threads\":0.0,\"idle_time\":0.0,\"error_count\":0.0,\"message\":\"\",\"url\":\"\",\"ddsource\":\"jmeter\",\"sent_bytes\":124.0,\"thread_group\":\"bar\",\"body_size\":0.0,\"content_type\":\"\",\"load_time\":125.0,\"thread_name\":\"bar baz\",\"sample_end_time\":126.0,\"bytes\":12345.0,\"connect_time\":0.0,\"sample_count\":10.0,\"data_type\":\"\",\"all_threads\":0.0,\"data_encoding\":null}";
-        Assert.assertEquals(this.logsBuffer.get(0).toString(), expectedPayload);
+        JSONParser parser = new JSONParser(JSONParser.MODE_PERMISSIVE);
+        Assert.assertEquals(this.logsBuffer.get(0), parser.parse(expectedPayload));
     }
 
     @Test
